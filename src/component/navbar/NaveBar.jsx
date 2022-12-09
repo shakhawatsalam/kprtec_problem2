7109269d1b225059d4c4217facab939d42bbbfa0
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-12-09 20:15:34 +0600

    navBar is almost done

--- src/component/navbar/NaveBar.jsx
+++ src/component/navbar/NaveBar.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import './NavBar.css'
+import "./NavBar.css";
 import logo from "../../Assets/new-index-logo.svg";
 const NaveBar = () => {
-    return (
-        <>
-            <div className="container">
-                <nav className="navbar">
-                    <div className="logo">
-                        <img src={logo} alt="" />
-                    </div>
-                    <div className="nav_items">
-                        <a href="#" className="Log">Log in</a>
-                        <a href="#" className="join">Join for free</a>
-                    </div>
-
-                </nav>
-
+  return (
+    <>
+      <div className="Nav_container">
+        <div className='container'>
+          <nav className='navbar'>
+            <div className='logo'>
+              <img src={logo} alt='' />
             </div>
-        </>
-    );
+            <div className='nav_items'>
+              <a href='#' className='Log'>
+                Log in
+              </a>
+              <a href='#' className='join'>
+                Join for free
+              </a>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NaveBar;
