5f367f5ad1965fce38a9d016bd315847e65b6773
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-12-09 21:06:48 +0600

    header Section is almost done

--- src/component/herosection/HeroSection.jsx
+++ src/component/herosection/HeroSection.jsx
@@ -1,18 +1,27 @@
 import React from "react";
-import './HeroSection.css';
+import "./HeroSection.css";
+import HeaderImg from "../../Assets/csm_people-worker-laboratory_113e01cc4a.jpg";
 
 const HeroSection = () => {
-    return (
-        <>
-            <div className="gray_background"></div>
-            <header className="container">
-                <div className="header_container">
-                    <h1>hello</h1>
-                </div>
-
-            </header>
-        </>
-    );
+  return (
+    <>
+      <div className='gray_background'></div>
+      <header className='container'>
+        <div className='header_container'>
+          <div className='heder_content'>
+            <h1>
+              Discover scientific knowledge and stay connected to the world of
+              science
+            </h1>
+            <button>Join for free</button>
+          </div>
+          <div className='header_img'>
+              <img src={HeaderImg} alt='' />
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default HeroSection;
