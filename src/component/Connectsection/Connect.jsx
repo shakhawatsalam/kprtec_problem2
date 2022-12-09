05d72ac59f04f27c462f5804e229f58b2bf00f25
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-12-10 00:59:33 +0600

    working on keywords button

--- src/component/Connectsection/Connect.jsx
+++ src/component/Connectsection/Connect.jsx
@@ -5,29 +5,50 @@ const Connect = () => {
   return (
     <>
       <div className='container'>
-              <div className='connect_container'>
-                  <div className="Connect_content">
-                      <h1>Connect with your scientific community</h1>
-                      <p>
-                      Share your research, collaborate with your peers, and get the support you need to advance your career.
-                      </p>
-                  </div>
-                      <div className="button_content">
-                          <h5>VISIT TOPIC PAGES</h5>
-                          <div className="buttons">
-                              <button>Engineering</button>
-                              <button>Engineering</button>
-                              <button>Engineering</button>
-                              <button>Engineering</button>
-                              <button>Engineering</button>
-                              <button>Engineering</button>
-                              <button>Engineering</button>
-                              <button>Engineering</button>
-                              <button>Engineering</button>
-                              <button>Engineering</button>
-                          </div>
-                      </div>
+        <div className='connect_container'>
+          <div className='Connect_content'>
+            <h1>Connect with your scientific community</h1>
+            <p>
+              Share your research, collaborate with your peers, and get the
+              support you need to advance your career.
+            </p>
+          </div>
+          <div className='button_content'>
+            <h5>VISIT TOPIC PAGES</h5>
+            <div className='buttons'>
+              <div>
+                <a href='#'>Engineering</a>
               </div>
+              <div>
+                <a href='#'>Mathematics</a>
+              </div>
+              <div>
+                <a href='#'>Biology</a>
+              </div>
+              <div>
+                <a href='#'>Computer Science</a>
+              </div>
+              <div>
+                <a href='#'>Climate Change</a>
+              </div>
+              <div>
+                <a href='#'>Medicine</a>
+              </div>
+              <div>
+                <a href='#'>Physics</a>
+              </div>
+              <div>
+                <a href='#'>Social Science</a>
+              </div>
+              <div>
+                <a href='#'>Astrophysics</a>
+              </div>
+              <div>
+                <a href='#'>Chemistry</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,8 @@ const Home = () => {
             <NaveBar />
             <HeroSection />
             <DiscoverResearch />
-            <Connect/>
+            <Connect />
+            <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid sit assumenda totam qui iure corrupti deleniti placeat culpa vel, id esse dolor ullam earum! Quibusdam illo blanditiis vitae totam optio atque, delectus voluptatem reiciendis ratione excepturi, cumque ad odit possimus aliquid? Sint consequatur labore aliquam, velit similique maiores doloribus explicabo.</h1>
         </>);
 };
 
