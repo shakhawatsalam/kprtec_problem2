1de6646c820ac8fda450123b3ee902733d5b31f8
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-12-11 20:49:40 +0600

    working on footer section

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Footer.css";
+import HiringImg from "../../Assets/footer-hiring@2x.jpg";
+
 
 const Footer = () => {
   return (
@@ -7,7 +9,29 @@ const Footer = () => {
       <footer className='footer_container'>
         <div className='container'>
           <div className='footer_content'>
-            <h3>ResearchGate Business Solutions</h3>
+                      <h3>ResearchGate Business Solutions</h3>
+                      <div className="photo_content">
+                          <div className="Scientific">
+                              <div className="Simg">
+                                  <img src={HiringImg} alt="" />
+                              </div>
+                              <div className="Scontent">
+                                  <h3>Scientific Recruitment</h3>
+                                  <p>Hire qualified researchers and build the best teams in science.</p>
+                                  <button>Find out more</button>
+                              </div>
+                          </div>
+                          <div className="Scientific">
+                          <div className="Simg">
+                                  <img src={HiringImg} alt="" />
+                              </div>
+                              <div className="Scontent">
+                                  <h3>Scientific Recruitment</h3>
+                                  <p>Hire qualified researchers and build the best teams in science.</p>
+                                  <button>Find out more</button>
+                              </div>
+                          </div>
+                      </div>
           </div>
         </div>
       </footer>
