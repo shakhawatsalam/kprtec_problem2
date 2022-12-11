15581d111fcc9d92b689bf54fd1fa5133d7f5e46
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-12-11 21:46:22 +0600

    full web site is almost done

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -38,31 +38,30 @@ const Footer = () => {
                 </div>
               </div>
             </div>
-                  </div>
-                  {/* about us section */}
-                  <div className="line"></div>
-                  <div className="About_content">
-                      <div className="about_us">
-                          <a href="#">About Us</a>
-                          <a href="#">About Us</a>
-                          <a href="#">About Us</a>
-                          <a href="#">About Us</a>
-                          <a href="#">About Us</a>
-                          <a href="#">About Us</a>
-                          <a href="#">About Us</a>
-                          <a href="#">About Us</a>
-                      </div>
-                      <div className="Copy_right">
-                          <a href="#">About Us</a>
-                          <a href="#">About Us</a>
-                          <a href="#">About Us</a>
-                          <a href="#">About Us</a>
-                          <a href="#">About Us</a>
-                          <a href="#">About Us</a>
-                          <a href="#">About Us</a>
-                          <a href="#">About Us</a>
-                      </div>
-                  </div>
+          </div>
+          {/* about us section */}
+          <div className='line'></div>
+          <div className='About_content'>
+            <div className='about_us'>
+              <a href='#'>About Us</a>
+              <a href='#'>News</a>
+              <a href='#'>Careers</a>
+              <a href='#'>Help Center</a>
+              <a href='#'>Terms</a>
+              <a href='#'>Privacy</a>
+              <a href='#'>Copyright</a>
+              <a href='#'>Imprint</a>
+            </div>
+            <div className='Copy_right'>
+              <a href='#'>Researchers</a>
+              <a href='#'>Q&A</a>
+              <a href='#'>Publications</a>
+              <a href='#'>Jobs</a>
+              <a href='#'>Advertising</a>
+              <a href='#'>Recruiting</a>
+              <p>© 2008-2022 ResearchGate GmbH. All rights reserved.</p>
+            </div>
+          </div>
         </div>
       </footer>
     </>
