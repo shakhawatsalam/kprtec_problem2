bc73d72ebe64aee3048ff366179947aeb1751ee5
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-12-11 20:20:28 +0600

    working on footer

---
+++ src/component/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className='footer_container'>
+        <div className='container'>
+          <div className='footer_content'>
+            <h3>ResearchGate Business Solutions</h3>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import AdvanceSection from "../component/Advanceyoursection/AdvanceSection";
 import Connect from "../component/Connectsection/Connect";
 import DiscoverResearch from "../component/Discoverscetion/DiscoverResearch";
+import Footer from "../component/footer/Footer";
 import HeroSection from "../component/herosection/HeroSection";
 import MeasureImpact from "../component/mesuresection/MeasureImpact";
 import NaveBar from "../component/navbar/NaveBar";
@@ -14,7 +15,8 @@ const Home = () => {
             <DiscoverResearch />
             <Connect />
             <MeasureImpact />
-            <AdvanceSection/>
+            <AdvanceSection />
+            <Footer/>
         </>);
 };
 
