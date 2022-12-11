fa25ae88ea5896670134cee23b727d2b2100fc8a
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-12-11 21:26:05 +0600

    working on footer content

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Footer.css";
 import HiringImg from "../../Assets/footer-hiring@2x.jpg";
-
+import MarketingImg from "../../Assets/footer-advertising@2x.jpg";
 
 const Footer = () => {
   return (
@@ -9,30 +9,60 @@ const Footer = () => {
       <footer className='footer_container'>
         <div className='container'>
           <div className='footer_content'>
-                      <h3>ResearchGate Business Solutions</h3>
-                      <div className="photo_content">
-                          <div className="Scientific">
-                              <div className="Simg">
-                                  <img src={HiringImg} alt="" />
-                              </div>
-                              <div className="Scontent">
-                                  <h3>Scientific Recruitment</h3>
-                                  <p>Hire qualified researchers and build the best teams in science.</p>
-                                  <button>Find out more</button>
-                              </div>
-                          </div>
-                          <div className="Scientific">
-                          <div className="Simg">
-                                  <img src={HiringImg} alt="" />
-                              </div>
-                              <div className="Scontent">
-                                  <h3>Scientific Recruitment</h3>
-                                  <p>Hire qualified researchers and build the best teams in science.</p>
-                                  <button>Find out more</button>
-                              </div>
-                          </div>
+            <h3>ResearchGate Business Solutions</h3>
+            <div className='photo_content'>
+              <div className='Scientific'>
+                <div className='Simg'>
+                  <img src={HiringImg} alt='' />
+                </div>
+                <div className='Scontent'>
+                  <h3>Scientific Recruitment</h3>
+                  <p>
+                    Hire qualified researchers and build the best teams in
+                    science.
+                  </p>
+                  <button>Find out more</button>
+                </div>
+              </div>
+              <div className='Scientific'>
+                <div className='Simg'>
+                  <img src={MarketingImg} alt='' />
+                </div>
+                <div className='Scontent'>
+                  <h3>Scientific Recruitment</h3>
+                  <p>
+                    Hire qualified researchers and build the best teams in
+                    science.
+                  </p>
+                  <button>Find out more</button>
+                </div>
+              </div>
+            </div>
+                  </div>
+                  {/* about us section */}
+                  <div className="line"></div>
+                  <div className="About_content">
+                      <div className="about_us">
+                          <a href="#">About Us</a>
+                          <a href="#">About Us</a>
+                          <a href="#">About Us</a>
+                          <a href="#">About Us</a>
+                          <a href="#">About Us</a>
+                          <a href="#">About Us</a>
+                          <a href="#">About Us</a>
+                          <a href="#">About Us</a>
                       </div>
-          </div>
+                      <div className="Copy_right">
+                          <a href="#">About Us</a>
+                          <a href="#">About Us</a>
+                          <a href="#">About Us</a>
+                          <a href="#">About Us</a>
+                          <a href="#">About Us</a>
+                          <a href="#">About Us</a>
+                          <a href="#">About Us</a>
+                          <a href="#">About Us</a>
+                      </div>
+                  </div>
         </div>
       </footer>
     </>
